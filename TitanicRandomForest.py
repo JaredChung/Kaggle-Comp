9b67348af85407529e1e44c5a13aae962f5efb83
JaredChung <Jared_chung@hotmail.com>
2015-10-24 12:24:27 +1100

    Update TitanicRandomForest.py

--- TitanicRandomForest.py
+++ TitanicRandomForest.py
@@ -9,67 +9,66 @@ from sklearn import svm
 
 # Data cleanup
 # TRAIN DATA
-train_df = pd.read_csv('train.csv', header=0)        # Load the train file into a dataframe
-
-# I need to convert all strings to integer classifiers.
-# I need to fill in the missing values of the data and make it complete.
+train_data = pd.read_csv('train.csv', header=0)        # Load the train file into a dataframe
 
 # female = 0, Male = 1
-train_df['Gender'] = train_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+
+
+train_data['Gender'] = train_data['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
 
 # Fill in missing Age with the Average
-train_df.loc[train_df.Age.isnull(),'Age'] = np.average(train_df.loc[train_df.Age.notnull()].Age)
+train_data.loc[train_data.Age.isnull(),'Age'] = np.average(train_data.loc[train_data.Age.notnull()].Age)
 
 
 # Embarked from 'C', 'Q', 'S'
 # Note this is not ideal: in translating categories to numbers, Port "2" is not 2 times greater than Port "1", etc.
 
 # All missing Embarked -> just make them embark from most common place
-if len(train_df.Embarked[ train_df.Embarked.isnull() ]) > 0:
-    train_df.loc[ train_df.Embarked.isnull(),'Embarked'] = train_df.Embarked.dropna().mode().values
+if len(train_data.Embarked[ train_data.Embarked.isnull() ]) > 0:
+    train_data.loc[ train_data.Embarked.isnull(),'Embarked'] = train_data.Embarked.dropna().mode().values
 
-Ports = list(enumerate((train_df['Embarked']).unique()))    # determine all values of Embarked,
+Ports = list(enumerate((train_data['Embarked']).unique()))    # determine all values of Embarked,
 Ports_dict = { name : i for i, name in Ports }              # set up a dictionary in the form  Ports : index
-train_df.Embarked = train_df.Embarked.map( lambda x: Ports_dict[x]).astype(int)     # Convert all Embark strings to int
+train_data.Embarked = train_data.Embarked.map( lambda x: Ports_dict[x]).astype(int)     # Convert all Embark strings to int
 
 # Remove the Name column, Cabin, Ticket, and Sex (since I copied and filled it to Gender)
-train_df = train_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'], axis=1) 
+train_data = train_data.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'], axis=1) 
 
 
 
 
 # TEST DATA
-test_df = pd.read_csv('test.csv', header=0)        # Load the test file into a dataframe
+test_data = pd.read_csv('test.csv', header=0)        # Load the test file into a dataframe
 
 # I need to do the same with the test data now, so that the columns are the same as the training data
 # I need to convert all strings to integer classifiers:
 # female = 0, Male = 1
-test_df['Gender'] = test_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+test_data['Gender'] = test_data['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
 
 # Embarked from 'C', 'Q', 'S'
 # All missing Embarked -> just make them embark from most common place
-if len(test_df.Embarked[ test_df.Embarked.isnull() ]) > 0:
-    test_df.Embarked[ test_df.Embarked.isnull() ] = test_df.Embarked.dropna().mode().values
+if len(test_data.Embarked[ test_data.Embarked.isnull() ]) > 0:
+    test_data.Embarked[ test_data.Embarked.isnull() ] = test_data.Embarked.dropna().mode().values
 # Again convert all Embarked strings to int
-test_df.Embarked = test_df.Embarked.map( lambda x: Ports_dict[x]).astype(int)
+test_data.Embarked = test_data.Embarked.map( lambda x: Ports_dict[x]).astype(int)
 
 #Fill in missing Age with Average
-test_df.loc[test_df.Age.isnull(),'Age'] = np.average(test_df.loc[test_df.Age.notnull(),'Age'])
+test_data.loc[test_data.Age.isnull(),'Age'] = np.average(test_data.loc[test_data.Age.notnull(),'Age'])
 
 
 #fill in missing Fare... Might need to change this
-test_df.loc[test_df.Fare.isnull(),'Fare'] = np.average(test_df.loc[test_df.Fare.notnull(),'Fare'])
+test_data.loc[test_data.Fare.isnull(),'Fare'] = np.average(test_data.loc[test_data.Fare.notnull(),'Fare'])
 
 # Collect the test data's PassengerIds before dropping it
-ids = test_df['PassengerId'].values
+ids = test_data['PassengerId'].values
 # Remove the Name column, Cabin, Ticket, and Sex (since I copied and filled it to Gender)
-test_df = test_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'], axis=1) 
+test_data = test_data.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'], axis=1) 
 
 
 # The data is now ready to go. So lets fit to the train, then predict to the test!
 # Convert back to a numpy array
-train_data = train_df.values
-test_data = test_df.values
+train_data = train_data.values
+test_data = test_data.values
 
 
 print 'Training...'
