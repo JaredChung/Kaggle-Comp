476d56f8e378437dfb1df64025bebd0a17d84ad1
JaredChung <Jared_chung@hotmail.com>
2015-11-06 13:13:49 +1100

    Update Walmart.py

--- Walmart.py
+++ Walmart.py
@@ -3,3 +3,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+#Some Basic Analysis
+
+train_data = pd.read_csv("train.csv")
+
+train_data['Weekday'].value_counts().plot(kind='bar') # Saturday and Sunday
+
