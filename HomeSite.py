8b71e861368c8d6fb7daf3c08d5310a409ffa2b6
JaredChung <Jared_chung@hotmail.com>
2015-11-18 14:59:17 +1100

    Create HomeSite.py

---
+++ HomeSite.py
@@ -0,0 +1,7 @@
+%matplotlib inline
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVC
+from sklearn import preprocessing
